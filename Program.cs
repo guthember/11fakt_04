a7b83994bf570622f8835e7482a7ecb40c2f48f7
Erik Gúth <guthember@gmail.com>
2019-09-11 16:29:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a;
+      int b;
+
+      Console.Write("First number: ");
+      a = Convert.ToInt32( Console.ReadLine());
+
+      Console.Write("Second number: ");
+      b = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine(a + "+" + b + "=" + (a + b));
+      Console.WriteLine(a + "-" + b + "=" + (a - b));
+      Console.WriteLine(a + "*" + b + "=" + (a * b));
+
+      Console.ReadKey();
+    }
+  }
+}
